e6c3bf671da808581e5fe40b484b3e7f111b1b7f
jainil04 <33036617+jainil04@users.noreply.github.com>
2025-10-04 01:50:54 -0700

    [PATCH] Add new images for wheelchairs, knee scooters, rollators, and transport chairs

--- src/components/custom/ProductCard.tsx
+++ src/components/custom/ProductCard.tsx
@@ -67,7 +67,7 @@ export const ProductCard = ({
             src={data.image}
             alt={data.name}
             fill
-            className="object-cover transition-transform duration-500 group-hover:scale-105"
+            className="object-contain transition-transform duration-500 group-hover:scale-105"
             sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
           />
 
