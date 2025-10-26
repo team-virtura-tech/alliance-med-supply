58f5d24f7eab0349e29bf4d12be781b284b807b9
Pritesh Patel <pritesh.patel52@outlook.com>
2025-10-25 23:28:54 -0700

    add cta and area section (#35)

--- src/components/custom/index.ts
+++ src/components/custom/index.ts
@@ -1,4 +1,5 @@
 export { ContactForm, type ContactFormProps } from './ContactForm';
+export { CtaSection, type CtaSectionProps } from './cta-section';
 export {
   ProductCard,
   type ProductCardData,
@@ -7,3 +8,7 @@ export {
 export { ProductCategoryPage } from './ProductCategoryPage';
 export { ProductDetailPage } from './ProductDetailPage';
 export { ProductsPage } from './ProductsPage';
+export {
+  ServiceAreaSection,
+  type ServiceAreaSectionProps,
+} from './service-area-section';
