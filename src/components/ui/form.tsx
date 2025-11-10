104c1bbc38ef91f6567481182c8f3e7de555ad9f
Pritesh Patel <pritesh.patel52@outlook.com>
2025-11-09 20:44:16 -0800

    fix form error

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -97,7 +97,7 @@ function FormLabel({
     <Label
       data-slot="form-label"
       data-error={!!error}
-      className={cn('data-[error=true]:text-destructive', className)}
+      className={className}
       htmlFor={formItemId}
       {...props}
     />
